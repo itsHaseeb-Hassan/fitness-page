d395ced65c1aaac2842c3937702bb396714d8af2
haseeb hassan <its.haseebhassan07@gmail.com>
2024-05-23 11:42:57 +0500

    navbar responsive and page completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,43 @@
-import React,{useState} from 'react'
+
+import React, { useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
+
 const NavBar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleNavbar = () => {
-        setIsOpen(!isOpen);
-    }
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="bg-white fixed top-0 w-full z-50 shadow-md" role="navigation">
-    <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between py-4">
-            <Link to="#home" className="text-[20px] font-bold uppercase">Fitness</Link>
-            <button
-                className="flex flex-col justify-center items-center w-8 h-8 border border-gray-400 rounded md:hidden"
-                onClick={toggleNavbar}
-            >
-                <span className="block w-6 h-0.5 bg-gray-800 mb-1"></span>
-                <span className="block w-6 h-0.5 bg-gray-800 mb-1"></span>
-                <span className="block w-6 h-0.5 bg-gray-800"></span>
-            </button>
-            <div className={`${isOpen ? 'block' : 'hidden'} md:flex md:items-center w-full md:w-auto`} id="navbar-collapse">
-                <ul className="flex flex-col md:flex-row md:space-x-4 md:mt-0">
-                    <li><Link to="#home" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Home</Link></li>
-                    <li><Link to="#overview" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">About</Link></li>
-                    <li><Link to="#trainer" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Trainers</Link></li>
-                    <li><Link to="#blog" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Blog</Link></li>
-                    <li><Link to="#price" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Prices</Link></li>
-                    <li><Link to="#testimonial" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Testimonials</Link></li>
-                </ul>
-            </div>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white uppercase">Fitness</span>
+        <button
+          onClick={toggleNavbar}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded={isOpen ? 'true' : 'false'}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
+        <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+        <ul className="flex flex-col md:flex-row md:space-x-4 md:mt-0">
+                     <li><Link to="#home" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Home</Link></li>
+                     <li><Link to="#overview" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">About</Link></li>
+                     <li><Link to="#trainer" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Trainers</Link></li>
+                     <li><Link to="#blog" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Blog</Link></li>
+                     <li><Link to="#price" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Prices</Link></li>
+                     <li><Link to="#testimonial" className="block py-2 px-4 text-gray-400 hover:text-gray-900 font-bold text-[20px] ">Testimonials</Link></li>
+                 </ul>
         </div>
-    </div>
-</div>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
